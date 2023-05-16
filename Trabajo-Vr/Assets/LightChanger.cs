271af16046a15c5bcf6c06db688174e7f87fe6a1
Pietrovp1001 <pietro.villano@upb.edu.co>
2023-05-16 11:40:34 -0500

    Luces y carteles "snapzones arreglados"

--- Trabajo-Vr/Assets/LightChanger.cs
+++ Trabajo-Vr/Assets/LightChanger.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class LightChanger : MonoBehaviour
 {
    public Light luzSalaInicial;
+   public Light luzSecundaria1,luzSecundaria2,luzSecundaia3,luzSecundaria4,luzSecundaria5;
    
    
    public void ChangeLight()
    {
        luzSalaInicial.color = Color.green;
+       luzSecundaria1.color = Color.green;
+       luzSecundaria2.color = Color.green;
+       luzSecundaia3.color = Color.green; 
+       luzSecundaria4.color = Color.green;
+       luzSecundaria5.color = Color.green;
    }
 }

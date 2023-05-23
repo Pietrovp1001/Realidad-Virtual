23690e352c81b52d6dfdd03d3975cc07146b7882
Pietrovp1001 <pietro.villano@upb.edu.co>
2023-05-23 10:54:54 -0500

    sprites puerta

--- Trabajo-Vr/Assets/LightChanger.cs
+++ Trabajo-Vr/Assets/LightChanger.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Tilia.Interactions.Interactables.Interactables;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Unity.UI;
+using Image = UnityEngine.UI.Image;
 
 public class LightChanger : MonoBehaviour
 {
    public Light luzSalaInicial;
    public Light luzSecundaria1,luzSecundaria2,luzSecundaia3,luzSecundaria4,luzSecundaria5;
-   
-   
+   [SerializeField] private Image spritePuertsIzquierda, spritePuertaDerecha;
+   [SerializeField] private Sprite  nuevospriteIzquierda, nuevoSpriteDerecha;   
+   public InteractableFacade puertaIzquierda, puertaDerecha;
    public void ChangeLight()
    {
        luzSalaInicial.color = Color.green;
@@ -16,5 +21,11 @@ public class LightChanger : MonoBehaviour
        luzSecundaia3.color = Color.green; 
        luzSecundaria4.color = Color.green;
        luzSecundaria5.color = Color.green;
+       spritePuertaDerecha.color = Color.green;
+       spritePuertsIzquierda.color = Color.green;
+       spritePuertsIzquierda.sprite = nuevospriteIzquierda;
+       spritePuertaDerecha.sprite = nuevoSpriteDerecha;
+       puertaIzquierda.enabled = true;
+       puertaDerecha.enabled = true;
    }
 }
